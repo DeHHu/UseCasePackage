2b262df77a3e851af0c159f98458df651fc795f5
Denis Sadakov <denis@novado.ru>
2022-10-31 11:48:03 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UseCasePackage
+
+A description of this package.
---
+++ Tests/UseCasePackageTests/UseCasePackageTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import UseCasePackage
+
+final class UseCasePackageTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(UseCasePackage().text, "Hello, World!")
+    }
+}
