d5f2f0f6cbee575d9ce2b08d8a4ce45ea809fe1f
Denis Sadakov <denis@novado.ru>
2022-10-31 19:51:39 +0300

    add promises

--- Sources/UseCasePackage/MainCase.swift
+++ Sources/UseCasePackage/MainCase.swift
@@ -6,13 +6,17 @@
 //
 
 import Foundation
+import Combine
 import MyFirstSwiftPackage
 
 public protocol MainCase {
 	func getMainText(id: String, completion: @escaping (Result<ProductModel, Error>) -> Void)
+	func getMainTextWithFuture(id: String) -> Future<ProductModel, Error>
 }
 
 public class MainCaseImpl: MainCase {
+
+	
 	
 	public init(){}
 	
@@ -27,5 +31,21 @@ public class MainCaseImpl: MainCase {
 			}
 		}
 		
+	}
+	
+	public func getMainTextWithFuture(id: String) -> Future<ProductModel, Error> {
+		
+		return Future<ProductModel, Error> { promise in
+			let repo: ProductsRepository = ProductsRepositoryImpl()
+			repo.product(by: id) { result in
+				switch result {
+					case .success(let success):
+						promise(.success(Mappers.productMapper(rawProduct: success)))
+					case .failure(let failure):
+						promise(.failure(failure))
+				}
+			}
+			
+		}
 	}
 }
